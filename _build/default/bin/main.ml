535a11f8c6c8e88893d0b96f361a21e5f37e794d
Yuvraj <yuvrajbiswalofficial@gmail.com>
2026-07-10 20:37:32 +0530

    fix: module name and test

---
+++ _build/default/bin/main.ml
@@ -0,0 +1,4 @@
+open Orray
+
+let v : Matrix.vector = [| 1.0; 2.0; 3.0 |]
+let result = Matrix.dot v v
--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,4 @@
-let () = print_endline "Hello, World!"
+open Orray
+
+let v : Vector.vector = [| 1.0; 2.0; 3.0 |]
+let result = Vector.dot v v
